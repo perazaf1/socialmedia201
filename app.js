8490bf4fb7090b7e7383e10acdeaa07625c9b1e5
perazaf1 <pe.razafindrakoto@gmail.com>
2021-03-28 11:59:11 +0200

    init js
    Signed-off-by: perazaf1 <pe.razafindrakoto@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const txtAnim = document.querySelector('h1')
 
-console.log(new TypeWriter(txtAnim));
+new TypeWriter(txtAnim, {
+    deleteSpeed: 20
+})
+    .typeString('Les réseaux sociaux')
+    .pauseFor(300)
+    .typeString('<strong>,')
+
 
 
 
